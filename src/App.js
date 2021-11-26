2c2436c8033b997baac5b11018ff3b9df391f853
PuneethKumarA <apuneethk@gmail.com>
2021-11-26 02:01:51 -0800

    useContext and useReducer to share comp state with other comps

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import React ,{useReducer}from 'react';
+import CompA from './components/contextNreducer/CompA';
+import CompB from './components/contextNreducer/CompB';
+import CompC from './components/contextNreducer/CompC';
 
-import React from 'react';
-import ClickCounter from './components/higherorderComponents/ClickCounter';
-import HoverCounter from './components/higherorderComponents/HoverCounter';
-import DocTitle from './components/customhooks/DocTitle';
-import DocTitle2 from './components/customhooks/DocTitle2';
-import Counter1 from './components/customhooks/functionality/Counter1';
-import Counter2 from './components/customhooks/functionality/Counter2';
+export const CountContext = React.createContext()
+const initialState = 0
 
-
-
-
-
-
+const reducer = (state,action) => {
+    switch(action){
+        case 'increment': return state+1
+        case 'decrement': return state-1
+        case 'reset': return initialState
+        default: return state
+    }
+}
 
 function App() {
+  const [count,dispatch] = useReducer(reducer,initialState)
   return (
-
+    <CountContext.Provider 
+              value = {  
+                        {countState: count, countDispatch: dispatch} 
+                       }>
     <div className="App">
-
-    <Counter1/>
-    <Counter2/>
+    Count in app.js is {count}
+      <CompA/>
+      <CompB/>
+      <CompC/>
     </div>
-
+    </CountContext.Provider>
   );
 }
+
 export default App;
