e477bb504c25726d8b0c071145af5bb5a8f471b0
PuneethKumarA <apuneethk@gmail.com>
2021-11-28 22:59:16 -0800

    two reducers to manage 2 different counters

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 
 import React from 'react';
-import CounterTwo from './components/reducerhook/CounterTwo';
+import CounterThree from './components/reducerhook/CounterThree';
 
 
 export const UserContext = React.createContext()     //exporting a context
@@ -13,7 +13,7 @@ export const BatchContext = React.createContext()
 function App() {
   return (
     <div className="App">
-    <CounterTwo/>
+    <CounterThree/>
     </div>
   );
 }
